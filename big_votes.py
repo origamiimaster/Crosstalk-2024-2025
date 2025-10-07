d688049aa884438e13e6a0f8e82531f7bf2eb417
Sebastian <foldit.com@gmail.com>
2025-10-07 17:04:09 -0400

    screening database

--- big_votes.py
+++ big_votes.py
@@ -6,6 +6,7 @@ from cuml import LinearRegression, LogisticRegression, Ridge, Lasso, ElasticNet
 import numpy as np
 from sklearn.ensemble import VotingRegressor
 from sklearn.linear_model import LinearRegression, Lasso, ElasticNet
+import pandas as pd
 
 
 def evaluate_model_with_wandb(model, X, y, cv_folds=5, project_name="crosstalk-sebastian-ivy"):
@@ -31,7 +32,7 @@ def evaluate_model_with_wandb(model, X, y, cv_folds=5, project_name="crosstalk-s
     # Cross-validation
     cv = StratifiedKFold(n_splits=cv_folds, shuffle=True, random_state=42)
     scores1 = cross_val_score(model, X, y, cv=cv, scoring='r2')
-    
+
     # scores2 = cross_val_score(model, X, y, cv=cv, scoring='accuracy')
     # scores3 = cross_val_score(model, X, y, cv=cv, scoring='top_k_accuracy')
 
@@ -40,11 +41,7 @@ def evaluate_model_with_wandb(model, X, y, cv_folds=5, project_name="crosstalk-s
         "mean_r2": np.mean(scores1),
         "std_r2": np.std(scores1),
         "min_r2": np.min(scores1),
-        "max_r2": np.max(scores1), 
-        # "mean_accuracy": np.mean(scores2),
-        # "std_accuracy": np.std(scores2),
-        # "min_accuracy": np.min(scores2),
-        # "max_accuracy": np.max(scores2), 
+        "max_r2": np.max(scores1),
     }
     wandb.log(metrics)
 
@@ -52,27 +49,18 @@ def evaluate_model_with_wandb(model, X, y, cv_folds=5, project_name="crosstalk-s
 
     for val in metrics:
         print(f"{val}: {metrics[val]:.4f}")
-    # print(f"Mean Accuracy: {np.mean(scores):.4f}")
-    # print(f"Std Accuracy: {np.std(scores):.4f}")
-    # print(f"Min Accuracy: {np.min(scores):.4f}")
-    # print(f"Max Accuracy: {np.max(scores):.4f}")
-    
 
     # Finish wandb run
     wandb.finish()
 
 
-
-
 if __name__ == "__main__":
-    train_dataset = MyDataset(filename="crosstalk_train.parquet", x_cols=('ECFP4', 'FCFP4')) 
-    # train_dataset = HashedHashDataset(filename="crosstalk_train.parquet", x_cols=('ECFP4', 'FCFP4')) 
+    train_dataset = MyDataset(filename="crosstalk_train.parquet", x_cols=('ECFP4', 'FCFP4'))
 
-    # model = RandomForestRegressor(random_state=0)
     r1 = cuRFR()
     r2 = cuRFR()
     r3 = cuRFR()
-    
+
     r4 = LinearRegression()
     # r5 = LogisticRegression()
     r6 = Ridge()
@@ -83,30 +71,31 @@ if __name__ == "__main__":
     r7 = Lasso()
     r8 = ElasticNet()
 
-    
     model = VotingRegressor([
-        ("tree1", r1), 
-        ("tree2", r2), 
-        ("tree3", r3), 
-        ("lr", r4), 
+        ("tree1", r1),
+        ("tree2", r2),
+        ("tree3", r3),
+        ("lr", r4),
         ("en", r8),
         ("lasso", r7),
         ("ridge", r6)
-        ], n_jobs=1)
-    
+    ], n_jobs=1)
 
     evaluate_model_with_wandb(model, train_dataset.X, train_dataset.y)
 
-    test_dataset = TestDataset(filename="crosstalk_test_20250305_inputs.parquet", x_cols=('ECFP4', 'FCFP4'))
-    print("Datasets Loaded")
+    # test_dataset = TestDataset(filename="crosstalk_test_20250305_inputs.parquet", x_cols=('ECFP4', 'FCFP4'))
+    # print("Datasets Loaded")
 
-    model.fit(train_dataset.X, train_dataset.y)
+    # model.fit(train_dataset.X, train_dataset.y)
 
-    output = model.predict(test_dataset.X)
-    print(output)
-    
-    with open("big_votes.txt", "w") as f:
-        for val in output:
-            f.write(str(val) + "\n")
-    
-    
+    # output = model.predict(test_dataset.X)
+    # print(output)
+
+    # with open("big_votes.txt", "w") as f:
+    #     for val in output:
+    #         f.write(str(val) + "\n")
+
+
+    screen_dataset = pd.read_csv("smiles.csv")
+
+    print(screen_dataset)
